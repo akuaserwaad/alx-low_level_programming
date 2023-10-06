8523516243e982dea974a6e1a9e593cabc347c0c
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-05 18:40:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - this is the main put function
- * Return: O
+ * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (O);
+		return (0);
 }	
