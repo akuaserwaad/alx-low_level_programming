b772dfc388f16540f3b77743ea63f3bd6beda49e
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-06 18:31:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Determines if a number is positive, negative or zero
+int main ()
+{ 
+	char alphabet = 'a';
+	while alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+
+	}
+	putchar('\n');
+	return (0);
+}
