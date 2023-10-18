24fd80959f0e3b98249fedf4f10827a9bb2242b0
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-18 14:49:25 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *leet - main function
+  *
+  * @n: Function parameter
+  *
+  *Return: value of n
+ */
+
+char *leet(char *n)
+{
+	int x, y;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (x = 0; n[x] != '\0'; x++)
+	{
+	for (y = 0; y < 10; y++)
+	{
+	if (n[x] == s1[y])
+	{
+	n[x] = s2[y];
+	}
+	}
+	}
+	return (n);
+}
