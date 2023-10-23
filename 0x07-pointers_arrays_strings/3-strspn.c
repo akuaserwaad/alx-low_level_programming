f862add0344f8e8bd2f2f3bc02110eb61c34270f
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-23 09:03:44 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strspn - main function
+  *
+  * @s: Function parameter
+  *
+  * @accept: Function parameter
+  *
+  * Return: Always 0.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int x = 0;
+	int y;
+
+	while (*s)
+	{
+	for (y = 0; accept[y]; y++)
+	{
+	if (*s == accept[y])
+	{
+	x++;
+	break;
+	}
+	else if (accept[y + 1] == '\0')
+	return (x);
+	}
+	s++;
+	}
+	return (x);
+}
