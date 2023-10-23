7f4b9c90d2f8c0c82b1cfbcbeaad2ce8303d6503
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-23 08:56:22 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _memcpy - main function prototype
+  *
+  * @src: Source parameter
+  *
+  * @dest: Destination parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: Copied memory
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+	{
+	dest[x] = src[x];
+	}
+	return (dest);
+}
