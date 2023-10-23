6d32cb51511238d72d8fc7beaed9114342ff2558
akuaserwaad <akuaserwaa93@gmail.com>
2023-10-23 09:07:39 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strpbrk - main function of the prototype
+  *
+  * @s: function parameter
+  *
+  * @accept: Fuunction parameter
+  *
+  * Return: Always 0.
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+	for (y = 0; accept[y] != '\0'; y++)
+	{
+	if (s[x] == accept[y])
+	return (s + x);
+	}
+	}
+	return (0);
+}
