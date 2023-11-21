daeb6727b037722a9698da58863b1acd185e47ca
akuaserwaad <akuaserwaa93@gmail.com>
2023-11-21 05:05:33 -0800

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the integer values of linked list
+ * @head: the head node
+ *
+ * Return: sum of the integer values
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
