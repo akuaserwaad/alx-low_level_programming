c26c0560e9a5822fb3b3c4679c947afd09ec2ded
akuaserwaad <141975577+akuaserwaad@users.noreply.github.com>
2024-01-15 23:28:02 +0000

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.github.com/opokuamaning/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+wget -P .. https://raw.github.com/akuaserwaad/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
